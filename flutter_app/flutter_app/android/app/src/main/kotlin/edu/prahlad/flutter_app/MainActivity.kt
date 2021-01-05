2de844079a8b6438679b3397752b8b53043ed25e
Prahlad Rao <prahladskingdom@hotmail.com>
2021-01-06 01:16:37 +0530

    Initial commit

---
+++ flutter_app/flutter_app/android/app/src/main/kotlin/edu/prahlad/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.prahlad.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_app/flutter_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(HelloFlutterApp());
+
+class HelloFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Text("Hello Flutter !"),
+      ),
+    );
+  }
+}
