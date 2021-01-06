50f35bec6716d90cf1a61f8dc1ab4678487466d1
Prahlad Rao <prahladskingdom@hotmail.com>
2021-01-06 23:39:08 +0530

    SafeArea & Scaffold Widget

--- flutter_app/flutter_app/lib/main.dart
+++ flutter_app/flutter_app/lib/main.dart
@@ -6,9 +6,24 @@ class HelloFlutterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: Text("Hello Flutter !"),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Hello Flutter!"),
+            leading: Icon(Icons.home),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.refresh),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          body: Center(
+            child: Text(
+              "Hello Flutter !",
+              style: TextStyle(fontSize: 24),
+            ),
+          )),
     );
   }
 }
