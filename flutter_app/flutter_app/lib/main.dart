9d364078d63a93e8c8b76073740f81080b642cf1
Prahlad Rao <prahladskingdom@hotmail.com>
2021-01-21 15:09:18 +0530

    Intro completed

--- flutter_app/flutter_app/lib/main.dart
+++ flutter_app/flutter_app/lib/main.dart
@@ -2,26 +2,46 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(HelloFlutterApp());
 
-class HelloFlutterApp extends StatelessWidget {
+class HelloFlutterApp extends StatefulWidget {
+  @override
+  _HelloFlutterAppState createState() => _HelloFlutterAppState();
+}
+
+String englishGreeting = "Hello Flutter !";
+String otherGreeting = "Hiyo Flutter !";
+
+class _HelloFlutterAppState extends State<HelloFlutterApp> {
+  String displayText = englishGreeting;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(
-            title: Text("Hello Flutter!"),
+            title: Text("Hello Flutter App"),
             leading: Icon(Icons.home),
             actions: [
               IconButton(
                 icon: Icon(Icons.refresh),
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    displayText =
+                        displayText == englishGreeting ?
+                        otherGreeting :
+                            englishGreeting;
+                  });
+                },
               ),
             ],
           ),
           body: Center(
             child: Text(
-              "Hello Flutter !",
-              style: TextStyle(fontSize: 24),
+              displayText,
+              //SOLUTION #2: Making Text Bold
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+
+              //SOLUTION #3: Making Text Bigger
+              //style: TextStyle(fontSize: 48, fontWeight: FontWeight.normal),
             ),
           )),
     );
