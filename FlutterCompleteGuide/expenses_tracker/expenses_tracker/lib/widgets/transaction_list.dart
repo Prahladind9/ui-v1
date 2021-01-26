f05e36823e8a46d0c7ce6623cde48084c58e7f03
Prahlad Rao <prahladskingdom@hotmail.com>
2021-01-26 22:00:16 +0530

    Using ListView for Transactions

--- FlutterCompleteGuide/expenses_tracker/expenses_tracker/lib/widgets/transaction_list.dart
+++ FlutterCompleteGuide/expenses_tracker/expenses_tracker/lib/widgets/transaction_list.dart
@@ -14,57 +14,57 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tx) {
-            return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          )),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\$${tx.amount}', //String Interpolation
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
+      //ListView.builder() - loads only the visible elements > saves memory > but have to maintain pagination
+      //ListView(children: []) - loads almost all the elements > leads to Memory Constraints
+      child: ListView(
+        children: transactions.map((tx) {
+          return Card(
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(
                           color: Colors.purple,
-                        ),
+                          width: 2,
+                        )),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '\$${tx.amount}', //String Interpolation
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
                       ),
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey.shade800,
-                          ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey.shade800,
                         ),
-                        Text(
-                          DateFormat.yMMMd().format(tx.date),
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey,
-                          ),
+                      ),
+                      Text(
+                        DateFormat.yMMMd().format(tx.date),
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
                         ),
-                      ],
-                    )
-                  ],
-                ));
-          }).toList(),
-        ),
+                      ),
+                    ],
+                  )
+                ],
+              ));
+        }).toList(),
       ),
     );
   }
